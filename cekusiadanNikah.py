c2a0c7ce1ea5050ee8d542b51de64d543d561ec6
fadilcirebon <93699606+fadilcirebon@users.noreply.github.com>
2022-01-02 11:59:43 -0800

    Initial commit

--- cekusiadanNikah.py
+++ cekusiadanNikah.py
@@ -4,7 +4,7 @@ usia = input('berapa usiamu sekarang?: ')
 if int(usia)< 18:
     print('masih kecil, belum siap menikah')
 elif int(usia)>60:
-    print('WADUH KETUAAN)
+    print('WADUH KETUAAN')
 else:
     print('SUDAH COCOK USIANYA NIH')
 
