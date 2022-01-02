91e6bce09edacb059b3a0732cc6a5ca3fda4d2dd
fadilcirebon <93699606+fadilcirebon@users.noreply.github.com>
2022-01-02 13:11:09 -0800

    Initial commit

--- Tuples list& Dictionary.py
+++ Tuples list& Dictionary.py
@@ -1,3 +1,26 @@
 coordinates = (1, 2, 3)
 x, y, z = coordinates
 print(y)
+
+customer = {
+    'name':'john',
+    'email':'johnwick@gmail.com',
+    'phone':'123456'}
+print(customer)
+
+phone= input('phone: ')
+digit_mapping   ={
+    '1':'one',
+    '2':'two',
+    '3':'three',
+    '4':'four',
+    '5':'five',
+    '6':'six',
+    '7':'seven'
+    }
+
+output=''
+for ph in phone:
+    output += digit_mapping.get(ph,'!')+' '
+
+print(output)
